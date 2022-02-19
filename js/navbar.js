4e9d110d66393431a234d338a8e19498fd51a4eb
Dafni Hernandez <dafni74gx@gmail.com>
2022-02-18 19:33:48 -0500

    Made style sheet for navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,9 @@
 directoryDepth = ".".repeat(window.location.href.replace(/[^/A]/g, "").length - 2)
 
 var menuStr =
-   `<a href="index.html">HOME</a>
-    <a href="work.html">WORK</a>
-    <a href="bio.html">BIO</a>`;
+   `<a href="">HOME</a>
+    <a href="work">WORK</a>
+    <a href="bio">BIO</a>`;
 
 var menuList = document.getElementById("menuBar");
 menuList.innerHTML = menuStr;
